cf8f2d9e6270bd95dd43bddbfbf3004cab218bca
zacum <mfilgyhty@hotmail.com>
2020-10-02 13:00:41 -0700

    branch 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -16,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello, World 14
         </a>
       </header>
     </div>
